360a79eeac35518799ab0924d84afc9bc5f3eed9
noir <zxj8911@hotmail.com>
2024-11-14 14:49:41 +0800

    add build step

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,18 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 3000
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      }
+    },
+    sourcemap: true,
+    assetsDir: 'assets',
+    brotliSize: false
   }
 })
